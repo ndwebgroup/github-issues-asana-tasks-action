e955340be02c2f06c1b211bfce4cdf60e92d5c3c
Joe Maller <joe@joemaller.com>
2024-03-18 09:59:03 -0400

    are we getting the token?

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // @ts-check
 
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 /**
  * Building from the docs here:
  * @link https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
  */
 try {
+  const TOKEN = core.getInput("ASANA_PAT");
   const payload = JSON.stringify(github.context.payload, null, 2);
   console.log(`The '${github.context.eventName}' event payload: ${payload}`);
+  console.log(`token length: ${TOKEN.length}`);
+  console.log(`munged token: ${TOKEN.replace(/[46]/g, "%")}`);
 } catch (error) {
   core.setFailed(error.message);
 }
